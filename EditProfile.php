defd20c4c63236a2187bbce7a548e6c22f41da79
erfan-atom <atompayeerapi@gmail.com>
2026-08-28 13:34:27 +0330

    update erfan

--- EditProfile.php
+++ EditProfile.php
@@ -63,7 +63,7 @@ if (isset($_POST['btnUpdate'])) {
                     `PhoneNumber` = ?,
                     `Address` = ?,
                     `Password` = ?
-                 WHERE UserName = ?"
+                 WHERE `UserName` = ?"
             );
 
             $Update->execute([
