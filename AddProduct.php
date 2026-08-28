09e927e2c8ed5a3445611fcdc1ab120c7511aad2
ghazal <ghzl86914@gmail.com>
2026-08-28 12:13:52 +0330

    update gazal

--- AddProduct.php
+++ AddProduct.php
@@ -54,7 +54,7 @@ if (isset($_POST['btnAddProduct'])) {
     if (count($Errors) == 0) {
 
         $stmt = $pdo->prepare("INSERT INTO tblproducts (UserName, ProductTitle, PriceProduct, ProductImageName)
-             VALUES ('$CurrentUserName', '$Title', '$Price', '$ImageName')"
+             VALUES (:CurrentUserName, :Title, :Price, :ImageName)"
             
         )
 
