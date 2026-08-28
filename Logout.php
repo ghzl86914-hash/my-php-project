a9d286d83d1e997f61f20e2fe3ac600758491fcb
erfan-atom <atompayeerapi@gmail.com>
2026-08-28 11:44:45 +0330

    update gazal

--- Logout.php
+++ Logout.php
@@ -2,38 +2,8 @@
 
 session_start();
 
-$host = 'localhost';
-$dbname = 'dbpanel';
-$dbuser = 'root';
-$dbpass = '';
+require "db.php";
 
-try{
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
-    $options = [
-        PDO::ATTR_ERRMODE =>
-        PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE =>
-        PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
-
-
-
-$pdo = new PDO(
-    $dsn,$dbuser,$dbpass,$options
-);
-
-//print("connected to database successfully");
-
-}
-
-catch(PDOException $e)
-{
-    error_log("database error ...".
-    $e->getmessage());
-    die("database connection field");
-}
 if (isset($_SESSION['Login'])) {
 
     $Username = $_SESSION['Login'];
