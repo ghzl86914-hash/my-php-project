fa60532046f93a545b3389b60353a91a2529a0e1
erfan-atom <atompayeerapi@gmail.com>
2026-08-31 21:56:03 +0330

    update erfan

--- user_manager.php
+++ user_manager.php
@@ -1,12 +1,14 @@
 <?php
 
-class user 
+class User 
 {
     private PDO $pdo;
 
     public function __construct(PDO $db_connection){
         $this->pdo = $db_connection;
     }
+
+    public function RegUser()
 }
 
 ?>
