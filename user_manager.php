2f3bc91f551a9fe5a9dc29a850027fd9fa6c16a4
ghazal <ghzl86914@gmail.com>
2026-08-31 22:38:19 +0330

    update gazal

--- user_manager.php
+++ user_manager.php
@@ -1,46 +1,38 @@
 <?php
 
-class User 
+class User
 {
     private PDO $pdo;
 
-    public function __construct(PDO $db_connection){
+    public function __construct(PDO $db_connection)
+    {
         $this->pdo = $db_connection;
     }
 
-    public function RegUser($Username,$Password,$Email,$FullName,$Address,$PhoneNumber)
+    public function RegUser($Username, $Password, $Email, $FullName, $PhoneNumber, $Address)
     {
-        $CheckUser = $this->$pdo->prepare("SELECT UserID FROM tblusers WHERE Username= ?");
+        $CheckUser = $this->pdo->prepare("SELECT UserID FROM tblusers WHERE Username= ?");
 
-        $CheckUser->execute([
-            $Username
-        ]);
+        $CheckUser->execute([$Username]);
 
-        $CheckUser->rowCount() > 0 
-        {
-            $checkuser = 1;
+        if ($CheckUser->rowCount() > 0) {
+            return false;
         }
 
-        if($checkuser > 0)
-        {
-                    $stmt = $this->$pdo->prepare("INSERT INTO tblusers
-                (`Username`,`Password`,`Email`,`FirstNameAndLastName`,`PhoneNumber`,`Address`)VALUES(?,?,?,?,?,?)");
+        $stmt = $this->pdo->prepare("INSERT INTO tblusers(`Username`,`Password`,`Email`,`FirstNameAndLastName`,`PhoneNumber`,`Address`)VALUES(?,?,?,?,?,?)");
 
-        return $stmt->execute([
-        $Username,
-        $Password,
-        $Email,
-        $FullName,
-        $PhoneNumber,
-        $Address
-        ]);  
-        }
+        return $stmt->execute([$Username, $Password, $Email, $FullName, $PhoneNumber, $Address]);
+    }
 
+    public function GetUser($Username)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM tblusers WHERE UserName = ?");
 
-         
+$stmt->execute([
+    $CurrentUserName
+]);
+
+return;
 
     }
-    
 }
-
-?>
