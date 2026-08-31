c34c7286f50313d623de70123de8a386b6e50ee1
erfan-atom <atompayeerapi@gmail.com>
2026-08-31 22:28:24 +0330

    update erfan

--- user_manager.php
+++ user_manager.php
@@ -8,7 +8,7 @@ class User
         $this->pdo = $db_connection;
     }
 
-    public function RegUser($Username,$Password,$Email,$FullName,$Address,$PhoneNumber)
+    public function RegUser($Username,$Password,$Email,$FullName,$PhoneNumber,$Address)
     {
         $CheckUser = $this->pdo->prepare("SELECT UserID FROM tblusers WHERE Username= ?");
 
