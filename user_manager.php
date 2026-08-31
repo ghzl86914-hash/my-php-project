05699bfd304222b468497e9373711f49524f092e
erfan-atom <atompayeerapi@gmail.com>
2026-08-31 22:10:03 +0330

    update erfan

--- user_manager.php
+++ user_manager.php
@@ -8,19 +8,19 @@ class User
         $this->pdo = $db_connection;
     }
 
-    public function RegUser($Username,$Password,$Email,$FullName,$Address,$PhoneNumber){
-        $this->$pdo;
-      $Connection = $this->prepare("INSERT INTO tblusers
-            (`Username`,`Password`,`Email`,`FirstNameAndLastName`,`PhoneNumber`,`Address`)VALUES(?,?,?,?,?,?)");
+    public function RegUser($Username,$Password,$Email,$FullName,$Address,$PhoneNumber)
+    {
+        $Connection = $this->pdo->prepare("INSERT INTO tblusers
+                (`Username`,`Password`,`Email`,`FirstNameAndLastName`,`PhoneNumber`,`Address`)VALUES(?,?,?,?,?,?)");
 
-    $Connection->execute([
-    $Username,
-    $Password,
-    $Email,
-    $FullName,
-    $PhoneNumber,
-    $Address
-    ]);  
+        $Connection->execute([
+        $Username,
+        $Password,
+        $Email,
+        $FullName,
+        $PhoneNumber,
+        $Address
+        ]);  
     }
     
 }
