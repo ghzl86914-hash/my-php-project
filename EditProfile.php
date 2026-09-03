538aa5fa0d1a5e7c32bb64062de9099290b26bc9
ghazal <ghzl86914@gmail.com>
2026-09-03 15:44:09 +0330

    update gazal

--- EditProfile.php
+++ EditProfile.php
@@ -62,6 +62,15 @@ if (isset($_POST['btnUpdate']))
 
             $resultadd = $UserManage->EditProfile($UserName,$FirstNameAndLastName,$Email,$PhoneNumber,$Address,$CurrentUserName,$HashedPassword);
             
+            if(!$resultadd === false)
+            {
+                $_SESSION['Login'] = $UserName;
+            echo "<script>alert('ویرایش با موفقیت انجام شد');</script>";
+            }
+            else
+            {
+                $Errors[] = "خطا در ذخیره اطلاعات";
+            }
         } 
         else 
         {
