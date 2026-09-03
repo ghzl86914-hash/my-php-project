b683da863b65b4aedc410c04fdbd4fe8e878d5d9
erfan-atom <atompayeerapi@gmail.com>
2026-09-03 15:54:25 +0330

    update erfan

--- EditProfile.php
+++ EditProfile.php
@@ -69,18 +69,6 @@ if (isset($_POST['btnUpdate']))
         {
             $Errors[] = "مقدار نباید خالی باشد!";
         }
-    
-        $resultadd = $UserManage->EditProfile($UserName,$FirstNameAndLastName,$Email,$PhoneNumber,$Address,$CurrentUserName);
-
-        if($resultadd)
-        {
-            $_SESSION['Login'] = $UserName;
-        echo "<script>alert('ویرایش با موفقیت انجام شد');</script>";
-        }
-        else
-        {
-            $Errors[] = "خطا در ذخیره اطلاعات";
-        }
 
     }     
 }
