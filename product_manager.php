2d5f9a9e6411f45ba4fc12fb2e215f638f22a4c4
ghazal <ghzl86914@gmail.com>
2026-09-06 15:05:22 +0330

    Add product_manager.php files from gazal

---
+++ product_manager.php
@@ -0,0 +1,20 @@
+<?php
+
+class product
+{
+    private PDO $pdo;
+
+    public function __construct(PDO $db_connection)
+    {
+        $this->pdo = $db_connection;
+    }
+
+    public function AddProduct($CurrentUserName,$Title,$Price,$ImageName)
+
+    $stmt = $this->pdo->prepare("INSERT INTO tblproducts (UserName, ProductTitle, PriceProduct, ProductImageName)
+             VALUES (?, ?, ?, ?)");
+
+             $stmt->execute([$CurrentUserName,$Title,$Price,$ImageName]);
+}
+
+?>
