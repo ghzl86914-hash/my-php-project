1d092ef7af195ea9fbd5333cbbdb89e26501884c
erfan-atom <atompayeerapi@gmail.com>
2026-09-08 22:25:06 +0330

    update erfan

--- modules/auth/login.php
+++ modules/auth/login.php
@@ -131,7 +131,7 @@ if (isset($_POST['login']) && empty($Errors))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../../asstes/css/Style.css">
+    <link rel="stylesheet" href="../../assets/css/Style.css">
 
 
     <meta name="theme-color" content="#712cf9" />
