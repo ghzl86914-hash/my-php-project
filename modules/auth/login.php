75dfa12cdee74ea1145171d428af40959a2935ad
ghazal <ghzl86914@gmail.com>
2026-09-08 20:06:35 +0330

    add admin from login php

--- modules/auth/login.php
+++ modules/auth/login.php
@@ -22,6 +22,7 @@ if (isset($_GET['back'])) {
 $Errors = [];
 $Username = '';
 $Password = '';
+$UserID = '';
 
 // بررسی قفل بودن
 if (isset($_SESSION['login_locked_until'])) {
@@ -74,10 +75,14 @@ if (isset($_POST['login']) && empty($Errors))
             $_SESSION['Login'] = $ResultSelect['UserName'];
             $_SESSION['Username'] = $ResultSelect['UserName'];
 
-            if($Username === "admin"){
+            if ($UserManage->IsAdmin($UserID)){
 
                 echo "<script>window.location='admin/admin_panel.php';</script>";
+
             }
+
+                
+            
 
             else{
 
