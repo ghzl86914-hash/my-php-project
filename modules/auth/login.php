93c0a9c3d5fe3cb1ca40537072f72b37a3445745
ghazal <ghzl86914@gmail.com>
2026-09-08 21:58:20 +0330

    update gazal

--- modules/auth/login.php
+++ modules/auth/login.php
@@ -130,8 +130,8 @@ if (isset($_POST['login']) && empty($Errors))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="assets/css/Style.css">
+    <link rel="stylesheet" href="../../bootstrap.min.css" />
+    <link rel="stylesheet" href="../../Style.css">
 
 
     <meta name="theme-color" content="#712cf9" />
