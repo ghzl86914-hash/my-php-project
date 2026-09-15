821743f83e598e226c6c49f71b2a59bc66264921
erfan-atom <atompayeerapi@gmail.com>
2026-09-15 14:52:55 +0330

    update erfan

--- modules/users/EditProfile.php
+++ modules/users/EditProfile.php
@@ -44,13 +44,12 @@ if (isset($_POST['btnUpdate']))
     }
 
     // اگر می‌خواد رمز عوض کنه
-    if (!empty($NewPassword)  && empty($CurrentPassword)) 
-    {
-        $Errors[] = "برای تغییر رمز، باید رمز فعلی را وارد کنید";
-    }
-    elseif (!password_verify($CurrentPassword, $User['Password'])) 
-    {
-        $Errors[] = "رمز فعلی اشتباه است";
+    if (!empty($NewPassword)) {
+        if ($CurrentPassword === '') {
+            $Errors[] = "برای تغییر رمز، باید رمز فعلی را وارد کنید";
+        } elseif (!password_verify($CurrentPassword, $User['Password'])) {
+            $Errors[] = "رمز فعلی اشتباه است";
+        }
     }
     
 
