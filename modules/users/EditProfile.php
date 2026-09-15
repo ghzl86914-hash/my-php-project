8f6e4f4905dbff928f11ecc701062e7757242ea3
ghazal <ghzl86914@gmail.com>
2026-09-15 15:03:13 +0330

    update edit profile

--- modules/users/EditProfile.php
+++ modules/users/EditProfile.php
@@ -64,7 +64,7 @@ if (isset($_POST['btnUpdate']))
         $HashedPassword = !empty($NewPassword) ? password_hash($NewPassword, PASSWORD_DEFAULT) : null;
         $timestamp = time();
 
-        $resultadd = $UserManage->EditProfile($UserName,$FirstNameAndLastName,$Email,$PhoneNumber,$Address,$CurrentUserName,$HashedPassword,$timestamp);
+        $resultadd = $UserManage->EditProfile($UserName,$FirstName,$LastName,$Email,$PhoneNumber,$Address,$CurrentUserName,$HashedPassword,$timestamp);
         
         if($resultadd)
         {
