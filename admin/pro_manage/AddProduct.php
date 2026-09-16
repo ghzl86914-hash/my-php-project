7eac05b05041dc87cc7999f6391bc0e783d211da
ghazal <ghzl86914@gmail.com>
2026-09-16 12:41:40 +0330

    update product

--- admin/pro_manage/AddProduct.php
+++ admin/pro_manage/AddProduct.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-require "config/db.php";
-require "classes/user_manager.php";
+require "../../config/db.php";
+require "../../classes/user_manager.php";
 
 $ProductManage = new  Product($pdo);
 
