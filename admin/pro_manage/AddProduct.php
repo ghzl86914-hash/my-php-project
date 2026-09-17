1c380faae36e50c7f25c455f4bf3a0febdddd63f
erfan-atom <atompayeerapi@gmail.com>
2026-09-17 16:40:55 +0330

    update erfan

--- admin/pro_manage/AddProduct.php
+++ admin/pro_manage/AddProduct.php
@@ -80,7 +80,7 @@ if (isset($_POST['btnAddProduct'])) {
     <title>افزودن محصول</title>
     <style>
         body {
-            font-family: Tahoma, Arial, sans-serif;
+            font-family: yekanbackh;
             background-color: #f2f4f8;
             direction: rtl;
             margin: 0;
