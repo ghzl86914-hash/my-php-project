7fdd2f5516ec06da90ef11f686bb04aae1c918f5
ghazal <ghzl86914@gmail.com>
2026-09-17 16:33:06 +0330

    update ? in product manager

--- classes/product_manager.php
+++ classes/product_manager.php
@@ -14,7 +14,7 @@ class Product
         $stmt = $this->pdo->prepare(
             "INSERT INTO product
             (`ProductTitle`, `PriceProduct`, `ProductImageName`, `ProductColor`, `Score`, `CategoryID`, `Stock`, `Description`, `Add_Date`, `BrandID`)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
         );
 
         return $stmt->execute([
