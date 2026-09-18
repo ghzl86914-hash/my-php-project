f212230cd2a1be5afe8ffe87c2cc11682ea25015
erfan-atom <atompayeerapi@gmail.com>
2026-09-18 15:31:47 +0330

    update erfan

--- admin/pro_manage/ProList.php
+++ admin/pro_manage/ProList.php
@@ -339,6 +339,10 @@ $Errors = [];
                         <!-- نمونه سطر ۱ (در پیاده‌سازی بک‌اند، این سطر داخل حلقه foreach قرار می‌گیرد) -->
                         <?php foreach($prolist as $pro):?>
                         
+                        <? 
+                            $catname = $ProductManage->CatName($pro['CategoryID']);
+                            $brandname = $ProductManage->BrandName($pro['$BrandID']);
+                        ?>
                         <tr>
                             <td><strong>#<?= $pro['ProductID']?></strong></td>
                             <td>
